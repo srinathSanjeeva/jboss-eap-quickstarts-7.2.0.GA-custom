2a315cf1906b4b0c7eeb69eb7916bbb2f033f1fe
srinathSanjeeva <20364513+srinathSanjeeva@users.noreply.github.com>
2020-03-31 18:43:36 -0400

    Updating client docker image.

--- jaxws-pojo/client/build-run-image.sh
+++ jaxws-pojo/client/build-run-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -vex
 
-docker build --tag=sanjeevas/jaxws-pojo-client:1.0 service/
+docker build --tag=sanjeevas/jaxws-pojo-client:1.0 client/
 
-docker run -d -p 8080:8080 -p 9990:9990 sanjeevas/jaxws-pojo-client:1.0
+docker run -d -p 8081:8080 -p 9991:9990 sanjeevas/jaxws-pojo-client:1.0
